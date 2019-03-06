a36dac545bdfcc8781cb0bc07aa298a814505fef
Your Name <you@example.com>
2019-03-06 20:50:20 +0100

    Enable CORS

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -16,11 +16,25 @@ namespace TodoApi
             Configuration = configuration;
         }
 
+        readonly string AllowAll = "_allowAll";
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(AllowAll,
+                    builder =>
+                    {
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials();
+                    });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.Configure<Settings>(
                 options =>
@@ -46,6 +60,9 @@ namespace TodoApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors(AllowAll);
+
 
             app.UseHttpsRedirection();
             app.UseMvc();
